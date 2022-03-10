bb44a672d70ae6c22d048c479553df5438b47d2f
Rachel Howell <rac777blue@hotmail.com>
2022-03-10 15:10:27 +0000

    refactored wrap method

--- wordwrap.rb
+++ wordwrap.rb
@@ -3,14 +3,10 @@ class Wrapper
     def wrap(string, num)
         return string if string.length < num
 
-        # "the top"
-        # if whitespace (3) is less than num (5) then range should be 0..whitespace
         whitespace = string[0...num].rindex(" ")
 
-        if whitespace && whitespace < num
-            return string[0..whitespace].strip + "\n" + string[whitespace..-1].strip
-        end
-        
-        return string[0..num-1].strip + "\n" + string[num..-1].strip
+        whitespace && whitespace < num ? range = whitespace : range = num
+
+        return string[0..range-1].strip + "\n" + string[range..-1].strip
     end
 end
